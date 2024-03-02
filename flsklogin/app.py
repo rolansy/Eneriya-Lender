5c4df81f88a54571d3ad4936e031dba91d475ef9
rolansy <ronalshoeygeorge@gmail.com>
2024-03-02 07:03:22 +0530

    Add registration and login forms to app.py

--- flsklogin/app.py
+++ flsklogin/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
 import os
 
 
@@ -11,12 +14,35 @@ db=SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'thisisasecretkey'
 
 
-class user(db.Model, UserMixin):
+class User(db.Model, UserMixin):
     id=db.Column(db.Integer, primary_key=True)
     username=db.Column(db.String(80),nullable=False, unique=True)
     password=db.Column(db.String(80),nullable=False)
 
 
+class RegisterForm(FlaskForm):
+    username=StringField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Username"})
+    password=PasswordField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Password"})
+    
+    submit=SubmitField("Register")
+
+    def validate_username(self, username):
+        existing_user_username=User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                "That username already exists. Please choose a different one."
+            )
+            
+class LoginForm(FlaskForm):
+    username=StringField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Username"})
+    password=PasswordField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Password"})
+    
+    submit=SubmitField("Login")
 
     
 @app.route('/')
