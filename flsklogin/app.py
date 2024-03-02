14323baa5f6e4831ca967cb0188ee9088424f15e
rolansy <ronalshoeygeorge@gmail.com>
2024-03-02 06:28:42 +0530

    Add SQLAlchemy, Flask-Login, and database configuration

--- flsklogin/app.py
+++ flsklogin/app.py
@@ -1,7 +1,24 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+import os
+
+
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db=SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'thisisasecretkey'
 
+
+class user(db.Model, UserMixin):
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(80),nullable=False, unique=True)
+    password=db.Column(db.String(80),nullable=False)
+
+
+
+    
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -17,3 +34,6 @@ def register():
 
 if __name__ =="__main__":
     app.run(debug=True)
+    
+with app.app_context(): 
+    db.create_all()
